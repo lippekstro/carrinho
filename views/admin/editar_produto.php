29bea3f2c4761d2c6514da76e25d65b7d63da1e2
lippekstro <fellipecastro13@hotmail.com>
2023-07-24 16:49:44 -0300

    mais bloqueio de acesso

--- views/admin/editar_produto.php
+++ views/admin/editar_produto.php
@@ -2,6 +2,13 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/carrinho/templates/cabecalho.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/carrinho/models/produto.php';
 
+if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['nv_acesso'] < 2) {
+    setcookie('msg', 'Você não tem permissão para acessar este conteúdo', time() + 3600, '/carrinho/');
+    setcookie('tipo', 'perigo', time() + 3600, '/carrinho/');
+    header('Location: /carrinho/index.php');
+    exit();
+}
+
 try {
     $produto = new Produto($_GET['id']);
 } catch (Exception $e) {
