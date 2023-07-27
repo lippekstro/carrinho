d3caf9843efadfec7278cf8da7945bb3df7d9dc3
lippekstro <fellipecastro13@hotmail.com>
2023-07-27 13:55:37 -0300

    ajuste espacamentos do rodape

--- templates/rodape.php
+++ templates/rodape.php
@@ -1,11 +1,7 @@
 </main>
 
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+<footer class="d-flex flex-wrap justify-content-between align-items-center p-3 my-4 border-top">
         <p class="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
-
-        <a href="/carrinho/index.php" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            <i class="bi bi-shop"></i>
-        </a>
 
         <ul class="nav col-md-4 justify-content-end">
             <li class="nav-item"><a href="mailto: exemplo@loja.com" class="nav-link px-2 text-body-secondary">Contate-nos</a></li>
