429f9125165b2774fe0cf3378e2c14202c27fb7f
lippekstro <fellipecastro13@hotmail.com>
2023-07-31 14:22:50 -0300

    ajuste nos cookies

--- views/admin/painel_controle.php
+++ views/admin/painel_controle.php
@@ -4,14 +4,14 @@ if (isset($_COOKIE['msg'])){
     setcookie('tipo', '', time() - 3600, '/carrinho/');
 }
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/carrinho/templates/cabecalho.php';
-
 if (!isset($_SESSION['usuario']) || $_SESSION['usuario']['nv_acesso'] < 2) {
     setcookie('msg', 'Você não tem permissão para acessar este conteúdo', time() + 3600, '/carrinho/');
     setcookie('tipo', 'perigo', time() + 3600, '/carrinho/');
     header('Location: /carrinho/index.php');
     exit();
 }
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/carrinho/templates/cabecalho.php';
 
 ?>
 
